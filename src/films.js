15b396451fb1a4d350c407b5b0e4454476185993
djura dzakula <djura.dzakula@gmail.com>
2022-01-10 17:06:21 +0100

    måndag

---
+++ src/films.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id :1,
+   bild :'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY209_CR0,0,140,209_AL_.jpg',
+   Title : 'Joker',
+   Director : 'Todd Phillips'
+
+},
+{
+  id :2,
+   bild :'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UY209_CR0,0,140,209_AL_.jpg',
+   Title : 'Avengers: Endgame',
+   Director : 'Anthony Russo'
+
+},
+{
+  id:3,
+   bild :'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UY209_CR0,0,140,209_AL_.jpg',
+   Title : 'Parasit',
+   Director : ' Bong Joon Ho'
+
+},
+{
+  id:4,
+   bild :'https://m.media-amazon.com/images/M/MV5BMTYzMDM4NzkxOV5BMl5BanBnXkFtZTgwNzM1Mzg2NzM@._V1_UY209_CR0,0,140,209_AL_.jpg',
+   Title : 'Toy Story 4',
+   Director : ' Josh Cooley '
+
+}
+];
